4163b4f317448aff0e3a9e190e483f7ad18851d0
Ariabatara <48705500+Ariabatara@users.noreply.github.com>
2021-08-29 06:58:49 +0700

    Add files via upload

---
+++ db_psb.sql
@@ -0,0 +1,90 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 29, 2021 at 01:56 AM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 8.0.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_psb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_admin`
+--
+
+CREATE TABLE `tb_admin` (
+  `id_admin` int(11) NOT NULL,
+  `nm_admin` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_admin`
+--
+
+INSERT INTO `tb_admin` (`id_admin`, `nm_admin`, `username`, `password`) VALUES
+(0, 'Aria', 'admin', '21232f297a57a5a743894a0e4a801fc3');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_pendaftaran`
+--
+
+CREATE TABLE `tb_pendaftaran` (
+  `id_pendaftaran` char(10) NOT NULL,
+  `tgl_daftar` date NOT NULL,
+  `thn_ajaran` varchar(9) NOT NULL,
+  `jurusan` varchar(50) NOT NULL,
+  `nm_peserta` varchar(50) NOT NULL,
+  `tmp_lahir` varchar(50) NOT NULL,
+  `tgl_lahir` date NOT NULL,
+  `jk` enum('Laki-Laki','Perempuan') NOT NULL,
+  `agama` varchar(15) NOT NULL,
+  `almt_peserta` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_pendaftaran`
+--
+
+INSERT INTO `tb_pendaftaran` (`id_pendaftaran`, `tgl_daftar`, `thn_ajaran`, `jurusan`, `nm_peserta`, `tmp_lahir`, `tgl_lahir`, `jk`, `agama`, `almt_peserta`) VALUES
+('P202100003', '2021-08-28', '2022/2023', 'IPA', 'Aria', 'Karawang', '1997-12-13', 'Laki-Laki', 'Islam', 'Telukambulu RT 02/01 ds. jayalaksana kec. cabangbungin kab. Bekasi'),
+('P202100004', '2021-08-28', '2022/2023', 'IPS', 'Batara', 'Jakarta', '2021-08-17', 'Laki-Laki', 'Islam', 'Jakarta');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_admin`
+--
+ALTER TABLE `tb_admin`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Indexes for table `tb_pendaftaran`
+--
+ALTER TABLE `tb_pendaftaran`
+  ADD PRIMARY KEY (`id_pendaftaran`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
